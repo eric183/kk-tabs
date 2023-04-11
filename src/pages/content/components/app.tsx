166b777646dffba3732a1e60541d7d34d3a3a397
一只眼圈酱 <yanquanjiang@yizhiyanquanjiangdeMacBook-Pro.local>
2023-04-11 14:32:15 +0800

    updated

--- src/pages/content/components/app.tsx
+++ src/pages/content/components/app.tsx
@@ -21,7 +21,7 @@ export default function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [searchingValue, setSearchingValue] = useState<string>("");
   const [tabs, setTabs] = useState<Tab[] | undefined>(undefined);
-
+  const [isSlash, setSlash] = useState<boolean>(false);
   const [seletedIndex, setSelectedIndex] = useState<number>(-1);
 
   const inputRef = useRef<HTMLInputElement>(null!);
@@ -29,15 +29,23 @@ export default function App() {
 
   const searchingTabs = useMemo(() => {
     let trueIndex = 0;
+
+    if (searchingValue.startsWith("/")) {
+      return tabs;
+    }
     return tabs?.map((tab) => {
       return {
         ...tab,
         children: tab.children.filter((f) => {
-          f.trueIndex = trueIndex;
-          trueIndex++;
-          return f.label
-            .toLocaleLowerCase()
-            .includes(searchingValue.toLocaleLowerCase());
+          if (
+            f.label
+              .toLocaleLowerCase()
+              .includes(searchingValue.toLocaleLowerCase())
+          ) {
+            f.trueIndex = trueIndex;
+            trueIndex++;
+            return true;
+          }
         }),
       };
     });
@@ -87,6 +95,32 @@ export default function App() {
     });
   };
 
+  const Badges = useMemo(() => {
+    if (!searchingValue.startsWith("/")) {
+      return false;
+    }
+
+    switch (searchingValue) {
+      case "/chat": {
+        return (
+          <span className="!ml-6 bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+            Chat
+          </span>
+        );
+      }
+      case "/draw": {
+        return (
+          <span className="!ml-6 bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-900 dark:text-purple-300">
+            Draw
+          </span>
+        );
+      }
+
+      default:
+        return false;
+    }
+  }, [searchingValue]);
+
   const adjustFontSize = () => {
     const htmlFontSize = parseFloat(
       getComputedStyle(document.documentElement).fontSize
@@ -106,7 +140,7 @@ export default function App() {
   };
 
   useEffect(() => {
-    queryTabs();
+    // queryTabs();
 
     document.addEventListener("keydown", handleKeyPress, false);
 
@@ -116,14 +150,12 @@ export default function App() {
   }, []);
 
   useEffect(() => {
+    queryTabs();
+
     setTimeout(() => {
       inputRef?.current?.focus();
     }, 100);
   }, [showModal]);
-
-  useEffect(() => {
-    console.log(seletedIndex, searchingTabs);
-  }, [seletedIndex, searchingTabs]);
 
   useLayoutEffect(() => {
     // adjustFontSize();
@@ -134,48 +166,6 @@ export default function App() {
       className={`kktab-app-content !fixed !inset-0 !z-[999] !overflow-y-hidden flex items-end justify-center ${
         showModal ? "flex" : "hidden"
       }`}
-      onKeyUp={(event) => {
-        console.log(event);
-        if (event.key.toLocaleLowerCase() === "enter") {
-          let currentTab;
-
-          searchingTabs.some(
-            (tab) =>
-              (currentTab = tab.children.find(
-                (c) => c.trueIndex === seletedIndex
-              ))
-          );
-
-          goTab(currentTab as unknown as TabChild);
-          // seletedIndex
-        }
-
-        if (event.key.toLocaleLowerCase() === "arrowup") {
-          if (seletedIndex >= 0) {
-            setSelectedIndex(() => seletedIndex - 1);
-
-            document
-              .querySelector(`[data-index="${seletedIndex - 1}"]`)
-              .scrollIntoView();
-          }
-        }
-
-        if (event.key.toLocaleLowerCase() === "arrowdown") {
-          const maxIndex = searchingTabs.reduce(
-            (pre, next) => pre + next.children.length,
-            0
-          );
-
-          if (seletedIndex < maxIndex - 1) {
-            setSelectedIndex(() => seletedIndex + 1);
-
-            document
-              .querySelector(`[data-index="${seletedIndex + 1}"]`)
-              .scrollIntoView();
-          }
-        }
-        // console.log(event.target.classList);
-      }}
     >
       <div
         className="!inset-0 !transition-opacity"
@@ -187,20 +177,38 @@ export default function App() {
       {/* 内容区 */}
 
       <div className="!absolute !w-3/5 !bg-gray-900 h-[50%] !z-30 !rounded-xl top-[10%] border border-gray-400 overflow-y-hidden flex flex-col kktab-app-list">
-        <div className="!w-full !relative !border-b">
+        <div className="!w-full !relative !border-b flex items-center">
           <MagnifyingGlassIcon className="!absolute !ml-3 !h-4 !top-1/2 !-translate-y-1/2 !text-white"></MagnifyingGlassIcon>
+          {/* to done */}
+          {/* {!searchingValue.startsWith("/") && (
+            <MagnifyingGlassIcon className="!absolute !ml-3 !h-4 !top-1/2 !-translate-y-1/2 !text-white"></MagnifyingGlassIcon>
+          )} */}
+          {/* {searchingValue.startsWith("/") ? (
+            { Badges }
+          ) : (
+            <MagnifyingGlassIcon className="!absolute !ml-3 !h-4 !top-1/2 !-translate-y-1/2 !text-white"></MagnifyingGlassIcon>
+          )} */}
+          {/* {Badges} */}
           <input
             ref={inputRef}
             className="!focus:outline-none !appearance-none !w-full !text-[14px] !leading-6 !text-white !placeholder-slate-400 !py-4 !pl-10 !bg-gray-900 !shadow-sm !rounded-xl !rounded-b-none"
             type="text"
             aria-label="Input a tab name"
             placeholder="Input a tab name..."
-            onChange={(evt: ChangeEvent<HTMLInputElement>) =>
-              setSearchingValue(evt.target.value)
-            }
+            onChange={(evt: ChangeEvent<HTMLInputElement>) => {
+              setSearchingValue(evt.target.value);
+
+              if (evt.target.value.startsWith("/")) {
+                setSlash(true);
+
+                return;
+              }
+
+              setSlash(false);
+            }}
             value={searchingValue}
             onKeyUp={(event) => {
-              console.log(event);
+              if (isSlash) return;
               if (event.key.toLocaleLowerCase() === "enter") {
                 let currentTab;
 
@@ -244,118 +252,80 @@ export default function App() {
           />
         </div>
 
-        <ul
-          className="flex-1 !overflow-scroll !mt-4 !px-2 !text-gray-200 flex flex-col ios-scroll min-h-min"
-          ref={scrollRef}
-        >
-          {searchingTabs &&
-            searchingTabs.map(
-              (tab, key) =>
-                tab.children.length > 0 && (
-                  <li key={key} className="pl-2 mb-2 flex-1">
-                    <Disclosure defaultOpen>
-                      {({ open }) => (
-                        <>
-                          <Disclosure.Button
-                            className="!text-left !mb-0 cursor-pointer"
-                            as="div"
-                          >
-                            <span className="!text-sm !text-gray-400">
-                              {tab.label}
-                            </span>
-                          </Disclosure.Button>
-                          <Disclosure.Panel
-                            className="!pt-2 !pb-2 !text-sm !text-white"
-                            as="ul"
-                          >
-                            {tab.children.map((t: TabChild, tk) => {
-                              return (
-                                <li
-                                  data-index={t.trueIndex}
-                                  key={tk}
-                                  className={`!flex !items-center !leading-10 mr-5 hover:bg-slate-200/25 !rounded-md !overflow-x-hidden truncate ${
-                                    seletedIndex === t.trueIndex
-                                      ? "bg-slate-200/25"
-                                      : ""
-                                  }`}
-                                >
-                                  <img
-                                    src={t.iconUrl}
-                                    className="rounded-2xl w-5 ml-1.5 mr-2"
-                                  />
-                                  <span
-                                    className="!w-full !cursor-pointer !text-left !whitespace-nowrap"
-                                    onClick={() => goTab(t)}
+        {isSlash ? (
+          <figure>
+            <ul className="kktabs-badges !mx-4 !my-2">
+              <li>
+                <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                  Chat
+                </span>
+              </li>
+            </ul>
+            <article></article>
+          </figure>
+        ) : (
+          <ul
+            className="flex-1 !overflow-scroll !mt-4 !px-2 !text-gray-200 flex flex-col ios-scroll min-h-min"
+            ref={scrollRef}
+          >
+            {searchingTabs &&
+              searchingTabs.map(
+                (tab, key) =>
+                  tab.children.length > 0 && (
+                    <li key={key} className="pl-2 mb-2">
+                      <Disclosure defaultOpen>
+                        {({ open }) => (
+                          <>
+                            <Disclosure.Button
+                              className="!text-left !mb-0 cursor-pointer"
+                              as="div"
+                            >
+                              <span className="!text-sm !text-gray-400">
+                                {tab.label}
+                              </span>
+                            </Disclosure.Button>
+                            <Disclosure.Panel
+                              className="!pt-2 !pb-2 !text-sm !text-white"
+                              as="ul"
+                            >
+                              {tab.children.map((t: TabChild, tk) => {
+                                return (
+                                  <li
+                                    data-index={t.trueIndex}
+                                    key={tk}
+                                    className={`!flex !items-center !leading-10 mr-5 hover:bg-slate-200/25 !rounded-md !overflow-x-hidden truncate ${
+                                      seletedIndex === t.trueIndex
+                                        ? "bg-slate-200/25"
+                                        : ""
+                                    }`}
                                   >
-                                    {t.label}
-                                  </span>
+                                    <img
+                                      src={t.iconUrl}
+                                      className="rounded-2xl w-5 ml-1.5 mr-2"
+                                    />
+                                    <span
+                                      className="!w-full !cursor-pointer !text-left !whitespace-nowrap"
+                                      onClick={() => goTab(t)}
+                                    >
+                                      {t.label}
+                                    </span>
 
-                                  {t.isPlaying && (
-                                    <SpeakerWaveIcon className="!ml-3 !h-4 !text-white" />
-                                  )}
-                                </li>
-                              );
-                            })}
-                          </Disclosure.Panel>
-                        </>
-                      )}
-                    </Disclosure>
-                  </li>
-                )
-            )}
-        </ul>
+                                    {t.isPlaying && (
+                                      <SpeakerWaveIcon className="!ml-3 !h-4 !text-white" />
+                                    )}
+                                  </li>
+                                );
+                              })}
+                            </Disclosure.Panel>
+                          </>
+                        )}
+                      </Disclosure>
+                    </li>
+                  )
+              )}
+          </ul>
+        )}
       </div>
     </div>
   );
-}
-
-// <div className="content-view fixed w-screen h-screen bg-black/80 z-[9999] top-0 left-0"></div>
-
-// export default function Example() {
-//   return (
-//     <div className="w-full px-4 pt-16">
-//       <div className="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
-
-//       </div>
-//     </div>
-//   )
-// }
-
-{
-  /* <Disclosure>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                      <span>What is your refund policy?</span>
-                      <ChevronUpIcon
-                        className={`${
-                          open ? "rotate-180 transform" : ""
-                        } h-5 w-5 text-purple-500`}
-                      />
-                    </Disclosure.Button>
-                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                      If you're unhappy with your purchase for any reason, email
-                      us within 90 days and we'll refund you in full, no
-                      questions asked.
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-              <Disclosure as="div" className="mt-2">
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                      <span>Do you offer technical support?</span>
-                      <ChevronUpIcon
-                        className={`${
-                          open ? "rotate-180 transform" : ""
-                        } h-5 w-5 text-purple-500`}
-                      />
-                    </Disclosure.Button>
-                    <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                      No.
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure> */
 }
